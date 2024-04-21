e0723bd2ac758e4f2eccac6ac0703d3cfb5d06e8
HuyenHuyencodoicocap <1632004huyen@gmail.com>
2024-04-21 11:37:09 +0700

    Update PanelEmployeeController.java

--- src/main/java/controller/PanelEmployeeController.java
+++ src/main/java/controller/PanelEmployeeController.java
@@ -65,6 +65,7 @@ public class PanelEmployeeController {
 		}
 		for(UserModel x : rowData) {
 			Vector<String> row = new Vector<>();
+			row.add(Integer.toString(x.getID()));
 			row.add(x.getFullName());
 			row.add(x.getEmail());
 			row.add(x.getUserName());
