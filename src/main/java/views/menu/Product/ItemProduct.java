23f3f72386fdfb529ddf208c5204a581098286e2
HuyenHuyencodoicocap <1632004huyen@gmail.com>
2024-04-22 21:41:42 +0700

    Update ItemProduct.java

--- src/main/java/views/menu/Product/ItemProduct.java
+++ src/main/java/views/menu/Product/ItemProduct.java
@@ -13,73 +13,72 @@ import javax.swing.text.PlainDocument;
 
 import model.ProductModel;
 
-
 public class ItemProduct extends JPanel {
 	JLabel centerItem;
 	JLabel southItem;
 
-	public ItemProduct(ProductModel model){
-		if(model==null) {
+	public ItemProduct(ProductModel model) {
+		if (model == null) {
 			this.setVisible(false);
-			return ;
+			return;
 		}
-		
+
 		centerItem = new TImage(new ImageIcon(model.getImage()));
 		southItem = new JLabel();
-		
+
 //		ImageIcon image=new ImageIcon(model.getImage());
 //		centerItem.setIcon(image);
 		centerItem.setOpaque(true);
 		centerItem.setBackground(Color.lightGray);
-//		if(model.getName().length()>=10 ) {
-//			String trimmedName = model.getName().substring(0, 17) + "...";
-//			southItem.setText("<html>"+"<html><div style=' overflow: hidden; text-overflow: ellipsis;'>" +
-//					"Tên: "+ trimmedName+ "<br>"+"Giá: "+ model.getPrice()+"</html>");
-//
-//		}else
-		southItem.setText("<html>"+"Tên: "+model.getName()+ "<br>"+"Giá: "+ model.getPrice()+"</html>");
-		
+		if (model.getName().length() > 17) {
+			String trimmedName = model.getName().substring(0, 17) + "...";
+			southItem.setText("<html><div style=\"overflow: hidden; text-overflow: ellipsis;white-space: nowrap;\">" +
+					"Tên: "+trimmedName+ "<br>"+"Giá: "+ model.getPrice()+"</html>");
+//			southItem.setText("<html>" + "Tên: " + model.getName() + "<br>" + "Giá: " + model.getPrice() + "</html>");
+
+		} else {
+			southItem.setText("<html>" + "Tên: " + model.getName() + "<br>" + "Giá: " + model.getPrice() + "</html>");
+		}
 		southItem.setForeground(Color.darkGray);
-		southItem.setFont(new Font("Times New roman",Font.PLAIN,20));
-		
+		southItem.setFont(new Font("Times New roman", Font.PLAIN, 20));
+
 		this.setLayout(new BorderLayout());
 		this.add(centerItem);
-		this.add(southItem,BorderLayout.SOUTH);
-		
+		this.add(southItem, BorderLayout.SOUTH);
+
 		this.addMouseListener(new MouseListener() {
-			
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				new insertUpdateDelete(model,false);
-				
-		}
+				new insertUpdateDelete(model, false);
+
+			}
 		});
-		
-	
+
 	}
 
 }
